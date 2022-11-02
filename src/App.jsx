519653b370870fce8873d5ce356936ce970e6191
musoev_ozodbek <musayev@gmail.com>
2022-11-02 18:05:55 +0500

    Thirdd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Home from './Home/Home'
 
 const App = () => {
   return (
-	<div>App</div>
+	<Home/>
   )
 }
 
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Five from "../../components/Five/Five";
-import Footer from "../../components/Footer/Footer";
-import Four from "../../components/Four/Four";
-import Header from "../../components/Header/Header";
-import Main from "../../components/Main/Main";
-import One from "../../components/One/One";
-import Finaly from "../../components/Finaly/Finaly";
-import Three from "../../components/Three/Three";
-import Two from "../../components/Two/Two";
+import Header from '../components/Header/Header'
+import Main from "../components/Main/Main"
+import One from "../components/One/One"
+import Two from "../components/Two/Two"
+import Three from "../components/Three/Three"
+import Four from "../components/Four/Four"
+import Five from "../components/Five/Five"
+import Finaly from "../components/Finaly/Finaly"
+import Footer from "../components/Footer/Footer"
 import "./home.css";
 const Home = () => {
   return (
     <div>
-      <Header />
+      <Header/>
       <Main />
       <One />
       <Two/>
